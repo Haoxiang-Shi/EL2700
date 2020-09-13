7ffc7c384887f25a73d071a6cc670f64317a34fb
Haoxiang Shi <shihaoxiang@outlook.com>
2020-09-13 22:49:03 +0200

    part1_no_dstr

--- task3.py
+++ task3.py
@@ -12,8 +12,8 @@ A, B, Bw, C = pendulum.get_discrete_system_matrices_at_eq()
 ctl = DLQR(A, B, C)
 
 # Get control gains
-K, P = ctl.get_lqr_gain(Q= np.diag([0, 0, 0, 0]), 
-                        R= 0)
+K, P = ctl.get_lqr_gain(Q= np.diag([1.0/100, 1.0/25, 1.0/0.03, 0/0.5]), 
+                        R=1.0/2)
 
 # Get feeforward gain
 lr = ctl.get_feedforward_gain(K)
@@ -22,52 +22,52 @@ lr = ctl.get_feedforward_gain(K)
 sim_env = EmbeddedSimEnvironment(model=pendulum, 
                                 dynamics=pendulum.discrete_time_dynamics,
                                 controller=ctl.feedfwd_feedback,
-                                time = 20)
+                                time = 10)
 sim_env.set_window(10)
 t, y, u = sim_env.run([0,0,0,0])
 
-# Part I - with disturbance
-pendulum.enable_disturbance(w=0.01)  
-sim_env_with_disturbance = EmbeddedSimEnvironment(model=pendulum, 
-                                dynamics=pendulum.discrete_time_dynamics,
-                                controller=ctl.feedfwd_feedback,
-                                time = 20)
-sim_env_with_disturbance.set_window(10)
-t, y, u = sim_env_with_disturbance.run([0,0,0,0])
+# # Part I - with disturbance
+# pendulum.enable_disturbance(w=0.01)  
+# sim_env_with_disturbance = EmbeddedSimEnvironment(model=pendulum, 
+#                                 dynamics=pendulum.discrete_time_dynamics,
+#                                 controller=ctl.feedfwd_feedback,
+#                                 time = 20)
+# sim_env_with_disturbance.set_window(10)
+# t, y, u = sim_env_with_disturbance.run([0,0,0,0])
 
 
-### Part II
-Ai, Bi, Bwi, Ci = pendulum.get_augmented_discrete_system()
-ctl.set_system(Ai, Bi, Ci)
-K, P = ctl.get_lqr_gain(Q= np.diag([0, 0, 0, 0]), 
-                        R= 0)
+# ### Part II
+# Ai, Bi, Bwi, Ci = pendulum.get_augmented_discrete_system()
+# ctl.set_system(Ai, Bi, Ci)
+# K, P = ctl.get_lqr_gain(Q= np.diag([0, 0, 0, 0]), 
+#                         R= 0)
 
-# Get feeforward gain              
-ctl.set_lr(lr)     
+# # Get feeforward gain              
+# ctl.set_lr(lr)     
 
-pendulum.enable_disturbance(w=0.01)  
-sim_env_with_disturbance = EmbeddedSimEnvironment(model=pendulum, 
-                                dynamics=pendulum.pendulum_augmented_dynamics,
-                                controller=ctl.lqr_ff_fb_integrator,
-                                time = 20)
-sim_env_with_disturbance.set_window(10)
-t, y, u = sim_env_with_disturbance.run([0,0,0,0,0])
+# pendulum.enable_disturbance(w=0.01)  
+# sim_env_with_disturbance = EmbeddedSimEnvironment(model=pendulum, 
+#                                 dynamics=pendulum.pendulum_augmented_dynamics,
+#                                 controller=ctl.lqr_ff_fb_integrator,
+#                                 time = 20)
+# sim_env_with_disturbance.set_window(10)
+# t, y, u = sim_env_with_disturbance.run([0,0,0,0,0])
 
-### Part III
-# Output feedback
-C = np.array([[1,0,0,0]])
-C = np.array([[1,0,0,0],
-              [0,0,1,0]])
+# ### Part III
+# # Output feedback
+# C = np.array([[1,0,0,0]])
+# C = np.array([[1,0,0,0],
+#               [0,0,1,0]])
 
-Qp = np.eye(4)
-Rn = np.eye(np.size(C,0))
-pendulum.set_kf_params(C,Qn,Rn)
-pendulum.init_kf()
+# Qp = np.eye(4)
+# Rn = np.eye(np.size(C,0))
+# pendulum.set_kf_params(C,Qn,Rn)
+# pendulum.init_kf()
 
-sim_env_with_disturbance_estimated = EmbeddedSimEnvironment(model=pendulum, 
-                                dynamics=pendulum.pendulum_augmented_dynamics,
-                                controller=ctl.lqr_ff_fb_integrator,
-                                time = 20)
-sim_env_with_disturbance_estimated.set_estimator(True)
-sim_env_with_disturbance_estimated.set_window(10)
-t, y, u = sim_env_with_disturbance_estimated.run([0,0,0,0,0])
+# sim_env_with_disturbance_estimated = EmbeddedSimEnvironment(model=pendulum, 
+#                                 dynamics=pendulum.pendulum_augmented_dynamics,
+#                                 controller=ctl.lqr_ff_fb_integrator,
+#                                 time = 20)
+# sim_env_with_disturbance_estimated.set_estimator(True)
+# sim_env_with_disturbance_estimated.set_window(10)
+# t, y, u = sim_env_with_disturbance_estimated.run([0,0,0,0,0])
