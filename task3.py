53d98f0bb6fadabdc71a2cbc1cbadd0137e669ea
BridgeQZH <zju_qzh@163.com>
2020-09-15 01:51:15 +0200

    p3

--- task3.py
+++ task3.py
@@ -6,8 +6,8 @@ from simulation import EmbeddedSimEnvironment
 
 # Create pendulum and controller objects
 PART1_DENUG = False
-PART2_DEBUG = True
-PART3_DEBUG = False
+PART2_DEBUG = False
+PART3_DEBUG = True
 pendulum = Pendulum()
 rho = 1
 # Get the system discrete-time dynamics
@@ -60,19 +60,26 @@ if (PART2_DEBUG == True):
 if (PART3_DEBUG == True):
     ### Part III
     # Output feedback
-    C = np.array([[1,0,0,0]])
+    Ai, Bi, Bwi, Ci = pendulum.get_augmented_discrete_system()
+    ctl.set_system(Ai, Bi, Ci)
+    K, P = ctl.get_lqr_gain(Q = np.diag([1.0 / (8.75 ** 2), 1.0 / (2.67 ** 2),\
+                                     1.0 / (0.189 ** 2), 1.0 / (0.32 ** 2), 1]), R = 1.0 / 4)
+    # C = np.array([[1,0,0,0]])
     C = np.array([[1,0,0,0],
                 [0,0,1,0]])
 
     Qp = np.eye(4)
-    Rn = np.eye(np.size(C,0))
-    pendulum.set_kf_params(C,Qn,Rn)
-    pendulum.init_kf()
+    # Qp = 50 * np.eye(4)
 
-    sim_env_with_disturbance_estimated = EmbeddedSimEnvironment(model=pendulum, 
-                                    dynamics=pendulum.pendulum_augmented_dynamics,
-                                    controller=ctl.lqr_ff_fb_integrator,
-                                    time = 20)
+    Rn = np.eye(np.size(C,0))
+    pendulum.set_kf_params(C,Qp,Rn)
+    pendulum.init_kf()
+    pendulum.enable_disturbance(w=0.01)  
+
+    sim_env_with_disturbance_estimated = EmbeddedSimEnvironment( model=pendulum, 
+                                                                 dynamics=pendulum.pendulum_augmented_dynamics,
+                                                                 controller=ctl.lqr_ff_fb_integrator,
+                                                                 time = 20)
     sim_env_with_disturbance_estimated.set_estimator(True)
-    sim_env_with_disturbance_estimated.set_window(10)
+    sim_env_with_disturbance_estimated.set_window(20)
     t, y, u = sim_env_with_disturbance_estimated.run([0,0,0,0,0])
