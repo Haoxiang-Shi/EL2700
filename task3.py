f2b38f5b15bec7ceba551e8ef8807cb12a1c8c7f
Haoxiang Shi <shihaoxiang@outlook.com>
2020-09-16 23:55:44 +0200

    part3 completed.

--- task3.py
+++ task3.py
@@ -4,11 +4,11 @@ from model import Pendulum
 from dlqr import DLQR
 from simulation import EmbeddedSimEnvironment
 
-ENABLE_AUGMENTED = False # False --> Part 1; True --> Part 2 & 3
+ENABLE_AUGMENTED = True # False --> Part 1; True --> Part 2 & 3
 ENABLE_DSTR = True # True --> Enable distrubance
 
-PART2_DEBUG = True
-PART3_DEBUG = False
+PART2_DEBUG = False
+PART3_DEBUG = True
 
 # Create pendulum and controller objects
 pendulum = Pendulum()
@@ -41,8 +41,8 @@ if ENABLE_AUGMENTED is False:
         sim_env_with_disturbance = EmbeddedSimEnvironment(model=pendulum, 
                                     dynamics=pendulum.discrete_time_dynamics,
                                     controller=ctl.feedfwd_feedback,
-                                    time = 10)
-        sim_env_with_disturbance.set_window(10)
+                                    time = 20)
+        sim_env_with_disturbance.set_window(20)
         t, y, u = sim_env_with_disturbance.run([0,0,0,0])
 
 if ENABLE_AUGMENTED is True:
@@ -50,7 +50,7 @@ if ENABLE_AUGMENTED is True:
     if PART2_DEBUG is True:
         Ai, Bi, Bwi, Ci = pendulum.get_augmented_discrete_system()
         ctl.set_system(Ai, Bi, Ci)
-        K, P = ctl.get_lqr_gain(Q= np.diag([1.0/100, 1.0/25, 1.0/0.03, 1.0/0.5, 1]), 
+        K, P = ctl.get_lqr_gain(Q= np.diag([1.0/100, 1.0/25, 20*1.0/0.03, 1.0/0.5, 1]), 
                             R= rho*1/4)
 
         # Get feeforward gain              
@@ -67,19 +67,29 @@ if ENABLE_AUGMENTED is True:
     ### Part III
     if PART3_DEBUG is True:
     # Output feedback
+        Ai, Bi, Bwi, Ci = pendulum.get_augmented_discrete_system()
+        ctl.set_system(Ai, Bi, Ci)
+        K, P = ctl.get_lqr_gain(Q= np.diag([1.0/100, 1.0/25, 20*1.0/0.03, 1.0/0.5, 1]), 
+                            R= rho*1/4)
+
+        # Get feeforward gain              
+        ctl.set_lr(lr)     
+
+        pendulum.enable_disturbance(w=0.01)
+
         C = np.array([[1,0,0,0]])
         C = np.array([[1,0,0,0],
                 [0,0,1,0]])
 
         Qp = np.eye(4)
         Rn = np.eye(np.size(C,0))
-        pendulum.set_kf_params(C,Qn,Rn)
+        pendulum.set_kf_params(C,Qp,Rn)
         pendulum.init_kf()
 
         sim_env_with_disturbance_estimated = EmbeddedSimEnvironment(model=pendulum, 
                                     dynamics=pendulum.pendulum_augmented_dynamics,
                                     controller=ctl.lqr_ff_fb_integrator,
-                                    time = 20)
+                                    time = 10)
         sim_env_with_disturbance_estimated.set_estimator(True)
         sim_env_with_disturbance_estimated.set_window(10)
         t, y, u = sim_env_with_disturbance_estimated.run([0,0,0,0,0])
